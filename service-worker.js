5bf983342928111a2f5c72a6af4d198e4c88f603
Shafiullah <shafiullahcrazyman@gmail.com>
2025-12-22 09:12:34 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'bananyum-cache-v1';
+
+const ASSETS_TO_CACHE = [
+  '/Flashify/',
+  '/Flashify/index.html',
+  '/Flashify/manifest.json',
+  '/Flashify/icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
